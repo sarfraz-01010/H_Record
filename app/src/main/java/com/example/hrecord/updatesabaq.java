9ab6a70315821c26f435b18cccdea102978518cc
sarfraz-01010 <sarfraz01010@gmail.com>
2023-06-21 14:43:55 +0500

    making objects of edittext and button

---
+++ app/src/main/java/com/example/hrecord/updatesabaq.java
@@ -0,0 +1,18 @@
+package com.example.hrecord;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class UpdateSabaq extends AppCompatActivity {
+
+    EditText rollno;
+    Button proceed;
+    boolean chk = false;
