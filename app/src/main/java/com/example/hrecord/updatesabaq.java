b0577800ddfa93181fbbbc6b60c71211dd3a893f
sarfraz-01010 <sarfraz01010@gmail.com>
2023-06-21 14:53:11 +0500

    xml file to update the record

--- app/src/main/java/com/example/hrecord/updatesabaq.java
+++ app/src/main/java/com/example/hrecord/updatesabaq.java
@@ -2,6 +2,7 @@ package com.example.hrecord;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,12 +12,13 @@ import android.widget.Toast;
 
 import java.util.List;
 
-public class UpdateSabaq extends AppCompatActivity {
+public class updatesabaq extends AppCompatActivity {
 
     EditText rollno;
     Button proceed;
     boolean chk = false;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,3 +28,36 @@ public class UpdateSabaq extends AppCompatActivity {
 
         rollno = findViewById(R.id.sabaqrollno);
         proceed = findViewById(R.id.next);
+
+        proceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String roll = rollno.getText().toString();
+                int r = Integer.parseInt(roll);
+
+                StudentsDbHelper studentsDbHelper = new StudentsDbHelper(updatesabaq.this);
+
+                List<Student> students = studentsDbHelper.selectAllStudents();
+
+                for(int i= 0; i<students.size();i++){
+                    Student s = students.get(i);
+                    if(s.getId() == r){
+                        chk = true;
+                        break;
+                    }
+                }
+
+                if(chk){
+                    Intent intent1 = new Intent(updatesabaq.this, updatesabaq2.class);
+                    intent1.putExtra("rollno", roll);
+                    startActivity(intent1);
+                }
+                else{
+                    Toast.makeText(updatesabaq.this, "No such student with this roll no.!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/hrecord/updatesabaq2.java
@@ -0,0 +1,14 @@
+package com.example.hrecord;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class updatesabaq2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_updatesabaq2);
+    }
+}
