3078de06f901695b62e1ccab7fd230fb7860521a
sarfraz-01010 <sarfraz01010@gmail.com>
2023-06-21 15:03:00 +0500

    taking info from the user to update the information

--- app/src/main/java/com/example/hrecord/updatesabaq2.java
+++ app/src/main/java/com/example/hrecord/updatesabaq2.java
@@ -1,14 +1,35 @@
 package com.example.hrecord;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class updatesabaq2 extends AppCompatActivity {
+import java.util.List;
 
+public class updateSabaq2 extends AppCompatActivity {
+
+    EditText sabaqSurah, sabaqStart, sabaqEnd, sabqi, manzil;
+    Button save;
+
+    //SabaqDbHelper sabaqDbHelper;
+    StudentsDbHelper studentsDbHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_updatesabaq2);
-    }
-}
+
+        Intent intent = getIntent();
+        String rollno = intent.getStringExtra("rollno");
+
+        sabaqSurah = findViewById(R.id.sabaqSurah);
+        sabaqStart = findViewById(R.id.sabaqStart);
+        sabaqEnd = findViewById(R.id.sabaqEnd);
+        sabqi = findViewById(R.id.sabqiSurah);
+        manzil = findViewById(R.id.manzilSurah);
+        save = findViewById(R.id.save);
